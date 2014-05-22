178d04a30dc9738ffa0146522ca397ecba12c556
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-05-22 14:10:19 -0400

    primero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carads::Application.config.secret_key_base = 'd1d52f24928435a480e93b2a572f817d15eb4d65927bb2ebd30b8eae8ab6cfb089d7de67bb4366e5d06d3219100342525f12014ffc4652aba75426c33b1dd85e'
